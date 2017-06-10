ff9413d3544758fa69545faceb2c087d2b775b31
phramusca <raphael.camus@free.fr>
2017-06-10 22:53:42 +0200

    Local player

--- app/src/main/java/phramusca/com/jamuzremote/Track.java
+++ app/src/main/java/phramusca/com/jamuzremote/Track.java
@@ -1,28 +1,44 @@
 package phramusca.com.jamuzremote;
 
+import android.media.MediaMetadataRetriever;
+
+import java.io.File;
+
 /**
  * Created by raph on 01/05/17.
  */
 public class Track {
+    private int id;
     private int rating;
     private String title;
     private String album;
     private String artist;
     private String coverHash;
+    private String path;
+    private String genre;
 
-    public Track(int rating, String title, String album, String artist, String coverHash) {
+    public Track(int id, int rating, String title, String album,
+                 String artist, String coverHash, String path, String genre) {
+        this.id = id;
         this.rating = rating;
         this.title = title;
         this.album = album;
         this.artist = artist;
         this.coverHash = coverHash;
+        this.genre=genre;
+        this.path = path;
     }
 
     @Override
     public String toString() {
-        return "<b>" + title + "</b><BR/>" + //NOI18N
-                "<i>" + album + "</i><BR/>" + //NOI18N
-                "" + artist + ""; //NOI18N
+        return   title + "<BR/>" +
+                artist + "<BR/>"+
+                album + "<BR/>"+
+                genre + "<BR/>";
+    }
+
+    public int getId() {
+        return id;
     }
 
     public String getCoverHash() {
@@ -31,5 +47,37 @@ public class Track {
 
     public int getRating() {
         return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAlbum() {
+        return album;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    //TODO: Use the same cache system as for remote
+    public byte[] getArt() {
+        MediaMetadataRetriever mmr = new MediaMetadataRetriever();
+        mmr.setDataSource(path);
+        byte[] art = mmr.getEmbeddedPicture();
+        return art;
     }
 }
