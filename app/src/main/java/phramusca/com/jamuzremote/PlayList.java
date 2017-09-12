c5844a0c287c430b5453574c0861442d380c2af1
phramusca <phramusca@gmail.com>
2017-09-12 21:39:42 +0200

    removed local bool + toggleButtonRandom

--- app/src/main/java/phramusca/com/jamuzremote/PlayList.java
+++ app/src/main/java/phramusca/com/jamuzremote/PlayList.java
@@ -7,20 +7,26 @@ public class PlayList {
 
     private String name;
     private String query;
+    private String order;
     MusicLibrary musicLibrary=null;
 
     public PlayList(String name) {
         this.name = name;
     }
 
-    public PlayList(String name, String query, MusicLibrary musicLibrary) {
+    public PlayList(String name, String query, String order, MusicLibrary musicLibrary) {
         this(name);
         this.query = query;
+        this.order = order;
         this.musicLibrary = musicLibrary;
     }
 
     public String getQuery() {
         return query;
+    }
+
+    public String getOrder() {
+        return order;
     }
 
     @Override
