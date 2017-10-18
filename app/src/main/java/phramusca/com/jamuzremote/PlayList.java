199e919d391affbd24cc9a14189bb06bfc788b4f
phramusca <raphael.camus@free.fr>
2017-10-18 23:47:43 +0200

    tag filtering

--- app/src/main/java/phramusca/com/jamuzremote/PlayList.java
+++ app/src/main/java/phramusca/com/jamuzremote/PlayList.java
@@ -1,5 +1,7 @@
 package phramusca.com.jamuzremote;
 
+import java.util.ArrayList;
+
 /**
  * Created by raph on 11/06/17.
  */
@@ -8,25 +10,68 @@ public class PlayList {
     private String name;
     private String query;
     private String order;
-    MusicLibrary musicLibrary=null;
+    private MusicLibrary musicLibrary=null;
+    private ArrayList<String> tags = new ArrayList<>();
+    private boolean includeUnTagged = true;
 
-    public PlayList(String name) {
+    public PlayList(String name, MusicLibrary musicLibrary) {
         this.name = name;
-    }
-
-    public PlayList(String name, String query, String order, MusicLibrary musicLibrary) {
-        this(name);
-        this.query = query;
-        this.order = order;
         this.musicLibrary = musicLibrary;
     }
 
-    public String getQuery() {
-        return query;
+    public PlayList(String name, MusicLibrary musicLibrary, ArrayList<String> tags) {
+        this(name, musicLibrary);
+        this.tags = tags;
     }
 
-    public String getOrder() {
-        return order;
+    public PlayList(String name, String query, String order, MusicLibrary musicLibrary) {
+        this(name, musicLibrary);
+        this.query = query;
+        this.order = order;
+    }
+
+    public ArrayList<Track> getTracks() {
+        if(query==null) {
+            return musicLibrary.getTracks(getWhere(tags));
+        } else {
+            return musicLibrary.getTracks(query, order);
+        }
+    }
+
+    /**
+     *
+     * @param value
+     */
+    public void toggleTag(String value) {
+        if(value.equals("null")) {
+            includeUnTagged=!includeUnTagged;
+        } else {
+            if(tags.contains(value)) {
+                tags.remove(value);
+            }
+            else {
+                tags.add(value);
+            }
+        }
+    }
+
+    private String getWhere(ArrayList<String> tags) {
+        String in = "";
+        //FIXME: When no tags, what tracks are returned ?
+        if(tags.size()>0) {
+            in += "WHERE tag.value IN (";
+            for(String entry : tags) {
+                in+="\""+entry+"\",";
+            }
+            in = in.substring(0, in.length()-1);
+            in += ")";
+            if(includeUnTagged) {
+                in += " OR tag.value IS NULL";
+            }
+        } else if(!includeUnTagged) {
+            in += "WHERE tag.value NOT NULL";
+        }
+        return in;
     }
 
     public String getName() {
@@ -35,6 +80,7 @@ public class PlayList {
 
     @Override
     public String toString() {
-        return musicLibrary==null?name:name+" ("+ musicLibrary.getNb(query) +")";
+        //FIXME: getNb for "Selected" playlist
+        return query==null?name:name+" ("+ musicLibrary.getNb(query) +")";
     }
 }
