442360da23cec6db252a1beb62f8711865558806
phramusca <raphael.camus@free.fr>
2020-04-20 21:47:27 +0200

    api key in keys.properties

--- app/src/main/java/phramusca/com/jamuzremote/YoutubeConnector.java
+++ app/src/main/java/phramusca/com/jamuzremote/YoutubeConnector.java
@@ -1,6 +1,7 @@
 package phramusca.com.jamuzremote;
 
 import android.content.Context;
+import android.content.res.AssetManager;
 import android.util.Log;
 
 import com.google.api.client.http.javanet.NetHttpTransport;
@@ -12,9 +13,11 @@ import com.google.api.services.youtube.model.SearchResult;
 import com.google.api.services.youtube.model.Thumbnail;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Properties;
 
 // Adapted from https://github.com/abhi5658/search-youtube
 
@@ -31,7 +34,7 @@ public class YoutubeConnector {
     //Developer API key a developer can obtain after creating a new project in google developer console
     //Developer has to enable YouTube Data API v3 in the project
     //Add credentials and then provide the Application's package name and SHA fingerprint
-    public static final String KEY = "API_KEY_FROM_GOOGLE_CLOUD_CONSOLE";
+    public static String KEY;
 
     //SHA1 fingerprint of APP can be found by double clicking on the app signing report on right tab called gradle
     public static final String SHA1 = "A9:F0:59:BF:34:49:FB:67:4A:C8:08:A6:EC:E2:AD:BE:3E:1C:06:61";
@@ -43,12 +46,20 @@ public class YoutubeConnector {
     private static final long MAXRESULTS = 25;
 
     public YoutubeConnector(Context context) {
-        youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), request -> {
-            request.getHeaders().set("X-Android-Package", PACKAGENAME);
-            request.getHeaders().set("X-Android-Cert",SHA1);
-        }).build();
-
         try {
+
+            //TODO: Read only once at app start
+            AssetManager am = context.getAssets();
+            InputStream inputStream = am.open("keys.properties");
+            Properties properties= new Properties();;
+            properties.load(inputStream);
+            KEY=properties.getProperty("youtube.key");
+
+            youtube = new YouTube.Builder(new NetHttpTransport(), new JacksonFactory(), request -> {
+                request.getHeaders().set("X-Android-Package", PACKAGENAME);
+                request.getHeaders().set("X-Android-Cert",SHA1);
+            }).build();
+
             // Define the API request for retrieving search results.
             query = youtube.search().list("id,snippet");
             query.setKey(KEY);
