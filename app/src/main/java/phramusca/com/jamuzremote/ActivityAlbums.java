46a004ccf8af81ca017b0fa6b4f8aecbb1f2f264
phramusca <raphael.camus@free.fr>
2021-10-31 13:49:41 +0100

    fix reset RepoAlbums

--- app/src/main/java/phramusca/com/jamuzremote/ActivityAlbums.java
+++ app/src/main/java/phramusca/com/jamuzremote/ActivityAlbums.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
@@ -69,29 +70,33 @@ public class ActivityAlbums extends AppCompatActivity {
         EditText queryText = findViewById(R.id.filter_album);
         queryText.addTextChangedListener(new TextWatcher() {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 searchQuery = s.toString();
                 adapterCursorAlbum.getFilter().filter(searchQuery);
-
             }
 
             @Override
-            public void afterTextChanged(Editable s) {
-
-            }
+            public void afterTextChanged(Editable s) { }
         });
-        queryText.setOnEditorActionListener(new DoneOnEditorActionListener());
+        queryText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                InputMethodManager inputMethodManager = (InputMethodManager) v.getContext()
+                        .getSystemService(Context.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                v.setVisibility(View.GONE);
+                return true;
+            }
+            return false;
+        });
 
         Button button_search = findViewById(R.id.button_search);
         button_search.setOnClickListener(v -> {
             queryText.setVisibility(View.VISIBLE);
             queryText.requestFocus();
-            final InputMethodManager inputMethodManager = (InputMethodManager) getApplicationContext()
+            final InputMethodManager inputMethodManager = (InputMethodManager) v.getContext()
                     .getSystemService(Context.INPUT_METHOD_SERVICE);
             inputMethodManager.showSoftInput(queryText, InputMethodManager.SHOW_IMPLICIT);
         });
