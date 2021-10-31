f6e648679850ed453ec5bfa9ba05160e82405d75
phramusca <phramusca@gmail.com>
2021-10-31 15:38:36 +0100

    minor clean

--- app/src/main/java/phramusca/com/jamuzremote/ServiceSync.java
+++ app/src/main/java/phramusca/com/jamuzremote/ServiceSync.java
@@ -4,6 +4,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.net.wifi.WifiManager;
 import android.os.PowerManager;
 import android.util.Log;
