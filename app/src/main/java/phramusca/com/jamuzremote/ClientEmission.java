221e0d7d096711ca404ad4f52ef87c87e2033563
Raphael Camus <raphael.camus.fr@non.se.com>
2022-10-05 23:16:44 +0200

    remove some useless comments

--- app/src/main/java/phramusca/com/jamuzremote/ClientEmission.java
+++ app/src/main/java/phramusca/com/jamuzremote/ClientEmission.java
@@ -1,12 +1,5 @@
 package phramusca.com.jamuzremote;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 import java.io.PrintWriter;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
